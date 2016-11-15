6ba3f919cc82da59a1c62aab8ade91a247021edc
redpepel <90-50@mail.ru>
2016-11-15 23:48:29 +0300

    Create RacioTimTable.java

---
+++ RacioTimTable.java
@@ -0,0 +1,21 @@
+package com.swampsnakes.racio;
+
+/**
+ * Created by User on 12.11.2016.
+ */
+public class RacioTimTable {
+    float work;
+    float study;
+    float traning;
+
+
+    float work1;
+    float study1;
+    float traning1;
+
+    ;
+
+
+
+
+}
