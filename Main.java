04d1ba5fecb5fa1d8ab9c6a52f9ba6bd6e4271ea
redpepel <90-50@mail.ru>
2016-12-03 01:49:21 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+package com.swampsnakes.racio;
+
+
+/**
+ * Created by User on 12.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+        Day day1 = new Day(); //Создается новый день и заполняется работой
+        day1.setArr1(10.1, 12.1);
+        day1.pr();
+
+       /* RacioTimTable RTT = new RacioTimTable ();
+        Day Til = new Day ();
+        System.out.println("Укажите количество часов, которое вы тратите на работу с твердым графиком и укажите точные часы работы. Я работаю с ");
+        RTT.work = 10;
+        System.out.println("До ");
+        RTT.work1 = 12;
+        Til.setArr1(12,15);
+        Til.pr();
+
+
+
+       /* System.out.println("Укажите количество часов, которое вы тратите на учебу с четким расписание и укажите точные часы работы");
+        RTT.study = in.nextFloat();
+        System.out.println("До ");
+        RTT.study1 = in.nextFloat();
+        System.out.println("Укажите количество часов, которое вы тратите на тренировку с твердым графиком и укажите точные часы работы");
+        RTT.traning = in.nextFloat();
+        System.out.println("До ");
+        RTT.traning1 = in.nextFloat(); */
+
+
+
+    }
+
+
+}
